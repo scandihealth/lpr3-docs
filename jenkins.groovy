3dc65072da0ca8bd6d6512fdb3ae76442f778c7a
Christian Peter Jonigkeit <cjonigkeit@csc.com>
2017-06-26 15:02:21 +0200

    Added first version

--- jenkins.groovy
+++ jenkins.groovy
@@ -1,4 +1,23 @@
 node {
-  stage('Environment') {
+  dir('master') {
+    stage('Checkout source') {
+      git credentialsId: 'scandihealth-git_lpr3-jenkins-master_github.com', url: 'git@github.com:scandihealth/lpr3-docs.git'
+    }
+    stage('Build') {
+      sh 'mkdocs build --clean'
+    }
+  }
+  dir('gh-pages') {
+    stage('Checkout destination') {
+      git branch: 'gh-pages', credentialsId: 'scandihealth-git_lpr3-jenkins-master_github.com', url: 'git@github.com:scandihealth/lpr3-docs.git'
+    }
+    stage('Collect build results') {
+     sh 'cp -r ../master/site/* .' 
+    }
+    stage('Publish') {
+      sh 'git add *'
+      sh 'git commit -m "new build"'
+      sh 'git push'
+    }
   }
 }
